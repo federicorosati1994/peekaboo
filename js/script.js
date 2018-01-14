30632daa39bfc47af9f54e91f611d1429b9c3b9f
Federico Rosati <federicorosati@MacBook-Pro-di-Federico.local>
2018-01-14 15:41:05 +0100

    bug

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ var swiper = new Swiper('.swiper-container', {
   	},
 });
 
-/*window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction()};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("scroller").style.width = scrolled + "%";
-}*/
+}
 
 new WOW().init();
